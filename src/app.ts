d6eb842deb1777638140a91ae868859cecb3d370
DesignDocJockey <hwong18@gmail.com>
2020-08-09 22:59:44 -0400

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import AWS from 'aws-sdk';
+import Kinesis from 'aws-sdk/clients/kinesis';
+
+// import individual service
+//import S3 from 'aws-sdk/clients/s3';
+
+console.log(`This is the beginning`);
+console.log(`Line 2`);
